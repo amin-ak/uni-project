525c7202f7fc773ae4f02d20dee05ef2a1a41622
= <=>
2017-07-01 23:51:42 +0430

    code_peygiri is editing

--- request.php
+++ request.php
@@ -1,3 +1,6 @@
+<?php
+    include('./db_conn.php');
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -17,16 +20,16 @@
             <h3 style="text-align:right;">
                         پیگیری نوبت
             </h3>
+            <form action="" method="post">
             <table>
                 <tr>
                     <td>کد پیگیری</td>
                    
                     <td>کد ملی</td>
                 </tr>
-                 
                 <tr>
                     <td>
-                        <input type="text" id="text" style="width:100px;">
+                        کد پیگیری<input type="text" id="text" name="code_peygiri" style="width:100px;">
                     </td>
                                     
 
@@ -39,8 +42,45 @@
                         <input type="submit" value="پیگیری ">
                     </td>
                 </tr>
+                 
+                
+                <?php
+                    if(isset($_POST['code_peygiri']))
+                    {
+                        $code_peygiri = $_POST['code_peygiri'];
+                        // echo "کد پیگیری".$code_peygiri;
+                        $sql = "select * from patient_tb WHERE code_melli='".$code_peygiri."'";
+                        $result = $db->query($sql);
+                        echo "<table border='1'><tr><td>نام بیمار</td>
+                                        <td>فامیلی بیمار</td>
+                                        <td>کد ملی</td>
+                                        <td>کد دکتر</td>
+                                        <td>وضعیت نوبت</td>
+                                    </tr>";
+                        while ($row = $result->fetch())
+                        {
+                            echo "
+                                        
+                                    <tr >
+                                        <td >".$row['name']."
+                                        </td>
+                                        <td >".$row['family']."
+                                        </td>
+                                        <td >".$row['code_melli']."
+                                        </td>
+                                        <td >".$row['doctor_code']."
+                                        </td>
+                                        <td >".$row['visit_time']."
+                                        </td>
+                                    </tr>
+                                ";
+                        }
+                    }
+                    echo "</table>";
+                ?>
+                
             </table>
-
+</form>
         </section>
         <aside id="category_sec">
             <?php include('./category.php') ?>    
